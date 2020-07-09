ce58cbaf8ef2dc752f393daec0cc43e28bffebde
Shipramzn <67088997+Shipramzn@users.noreply.github.com>
2020-07-09 22:12:20 +0530

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -2,17 +2,16 @@ const canvas= document.getElementById("myCanvas");
 const ctx= canvas.getContext("2d");
 const Height= canvas.height;
 const Width= canvas.width;
-var output;
+//var output;
 
 tileW= 20;
 tileH=20;
 
 tileRow= 20;
 tileColumn= 50;
-
-boundX=0;
-boundY=0;
-
+let set =[]
+let distance =[]
+let prev =[]
 
 var tiles=[];
 for (c=0; c < tileColumn; c++){
@@ -23,6 +22,8 @@ for (c=0; c < tileColumn; c++){
     }
 }
 function spot(c,r){
+    this.x = c*(tileW+3)
+    this.y = r*(tileH+3)
     this.c=c
     this.r =r
     this.state = 'e'
@@ -34,74 +35,23 @@ function spot(c,r){
         if(cur.c >0){
             neighbors.push(grid[c-1][r])
         }
-       if(cur.c < tileColumn-1){
+       if(cur.x < tileColumn-1){
         neighbors.push(grid[c+1][r])
-       }if(cur.r > 0){
+       }if(cur.y > 0){
         neighbors.push(grid[c][r-1])
-       }if(cur.r < tileRow-1){
-        neighbors.push(grid[c][r+1])
+       }if(cur.y < tileRow-1){
+        neighbors.push(grid[c][r+1 ])
        }
-        return neighbors;
+       return neighbors;
     }
+    
 }
 tiles[0][0].state= 's';  //s for start
 tiles[tileColumn-1][tileRow-1].state= 'f';    //f for finish
 
 start = tiles[0][0]
 end = tiles[tileColumn-1][tileRow-1]
-function removed_element(set,min_tile){
-   var index = set.indexOf(min_tile);
-   if (index > -1) {
-    set.splice(index, 1);
-}
-   
-}
-function solveMaze()
-{
 
-  let set =[]
-  let distance =[]
-  let prev =[]
-  distance[start]=0
-  set.push(start)
-  for(var c=0;c<tileColumn;c++){
-      for(var r=0;r<tileRow;r++){
-          distance[tiles[c][r]] = Infinity
-          prev[tiles[c][r]] = undefined
-
-      }
-  }
-  while(!set.empty()){
-      
-      let min_tile = min_dist_tile()
-      removed_element(set,min_tile)
-      let cur = min_tile
-      let neighbors = [] 
-      neighbors  = this.getNeighbors(cur)
-      neighbors.forEach(element => {
-         let alt = distance[cur] + heuristic(cur,element)
-         if(alt < distance[element]){
-             distance[element] = alt
-             pre[element]= cur
-             set.push(element)
-               
-         }
-      })
-}
-
-}
-function min_dist_tile(){
-  var min = Infinity
-    set.forEach(element =>{
-      if(set[element].g  < min){
-          min = set[element].g
-          min_tile = set[element]
-          min_tile.g = min
-      }
-    })
-    return min
-
-}
 
 function rect(x,y,w,h,state){
 
@@ -228,6 +178,58 @@ init();
 function display_alert(){
     alert('1. Green box shows the start\n2. Red box shows destination\n3. Draw obstacles and hit SOLVE\n4. Hit RESET if you want to play again' );
 }
+
+function removed_element(set,min_tile){
+    var index = set.indexOf(min_tile);
+    if (index > -1) {
+     set.splice(index, 1);
+ }
+    
+ }
+
+function solveMaze()
+{ 
+  distance[start]=0
+  set.push(start)
+  for(var c=0;c<tileColumn;c++){
+      for(var r=0;r<tileRow;r++){
+          distance[tiles[c][r]] = Infinity
+          prev[tiles[c][r]] = undefined
+
+      }
+  }
+  while(!set.empty()){
+      
+      let min_tile = min_dist_tile()
+       removed_element(set,min_tile)
+      let cur = min_tile
+      let neighbors= []
+      neighbors = this.getNeighbors(cur)
+      neighbors.forEach(element => {
+         let alt = distance[cur] + heuristic(cur,element)
+         if(alt < distance[element]){
+             distance[element] = alt
+             pre[element]= cur
+             set.push(element)
+               
+         }
+      })
+}
+
+}
+function min_dist_tile(){
+  var min = Infinity
+    set.forEach(element =>{
+      if(set[element].g  < min){
+          min = set[element].g
+          min_tile = set[element]
+          min_tile.g = min
+      }
+    })
+    return min
+
+}
+
 
 canvas.onmousedown= myDown;
 canvas.onmouseup= myUp;
